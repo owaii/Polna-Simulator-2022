ca5ea909eac95ada72b64f5c138f7a9c7ed51bba
owaii <118843083+owaii@users.noreply.github.com>
2022-12-16 11:24:49 +0100

    Very useful
    yes

---
+++ main.cpp
@@ -0,0 +1,106 @@
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int liczba;
+
+int main()
+{
+    setlocale(LC_CTYPE, "Polish");
+    cout << "Witaj mêdrco..." << endl;
+    Sleep(3000);
+    cout << "Pracujê nad gr¹ o technikum. \n";
+    Sleep(3000);
+    cout << "Niestety jestem leniwy i nawet nie zacz¹³em robiæ modeli... \n";
+    Sleep(3000);
+    cout << "Ale to nic, bo dalej mi siê chce, tylko nie mam czasu. \n";
+    Sleep(3000);
+    cout << "Wiêc jeœli ta gra wyjdzie dopiero za 4 lata, to siê nie dziw. \n";
+    Sleep(3000);
+    cout << "Ogólnie, ta gra bêdzie dzia³a³a na silniku Unity wiêc wszystko bêdzie w C# ale to teraz nie wa¿ne :). \n";
+    Sleep(5000);
+    cout << "Podaj swoj¹ ulubion¹ liczbê: ";
+    cin >> liczba;
+    if(liczba==2137, 420)
+    {
+        cout << "Pan kiedyœ stan¹³ nad brzegiem, \n";
+        Sleep(2000);
+        cout << "Szuka³ ludzi gotowych pójœæ za Nim; \n";
+        Sleep(2000);
+        cout << "By ³owiæ serca \n";
+        Sleep(2000);
+        cout << "S³ów Bo¿ych prawd¹. \n";
+        Sleep(3000);
+        cout << "O Panie, to Ty na mnie spojrza³eœ, \n";
+        Sleep(2000);
+        cout << "Twoje usta dziœ wyrzek³y me imiê. \n";
+        Sleep(2000);
+        cout << "Swoj¹ barkê pozostawiam na brzegu, \n";
+        Sleep(2000);
+        cout << "Razem z Tob¹ nowy zacznê dziœ ³ów. \n";
+        Sleep(3000);
+        cout << "Jestem ubogim cz³owiekiem, \n";
+        Sleep(2000);
+        cout << "Moim skarbem s¹ rêce gotowe \n";
+        Sleep(2000);
+        cout << "Do pracy z Tob¹ \n";
+        Sleep(2000);
+        cout << "I czyste serce. \n";
+        Sleep(3000);
+        cout << "O Panie, to Ty na mnie spojrza³eœ, \n";
+        Sleep(2000);
+        cout << "Twoje usta dziœ wyrzek³y me imiê. \n";
+        Sleep(2000);
+        cout << "Swoj¹ barkê pozostawiam na brzegu, \n";
+        Sleep(2000);
+        cout << "Razem z Tob¹ nowy zacznê dziœ ³ów. \n";
+        Sleep(3000);
+        cout << "Ty, potrzebujesz mych d³oni, \n";
+        Sleep(2000);
+        cout << "Mego serca m³odego zapa³em \n";
+        Sleep(2000);
+        cout << "Mych kropli potu \n";
+        Sleep(2000);
+        cout << "I samotnoœæi. \n";
+        Sleep(3000);
+        cout << "O Panie, to Ty na mnie spojrza³eœ, \n";
+        Sleep(2000);
+        cout << "Twoje usta dziœ wyrzek³y me imiê. \n";
+        Sleep(2000);
+        cout << "Swoj¹ barkê pozostawiam na brzegu, \n";
+        Sleep(2000);
+        cout << "Razem z Tob¹ nowy zacznê dziœ ³ów. \n";
+        Sleep(3000);
+        cout << "Dziœ wyp³yniemy ju¿ razem \n";
+        Sleep(2000);
+        cout << "£owiæ serca na morzach dusz ludzkich \n";
+        Sleep(2000);
+        cout << "Twej prawdy sieci¹ \n";
+        Sleep(2000);
+        cout << "I s³owem ¿ycia. \n";
+        Sleep(3000);
+        cout << "O Panie, to Ty na mnie spojrza³eœ, \n";
+        Sleep(2000);
+        cout << "Twoje usta dziœ wyrzek³y me imiê. \n";
+        Sleep(2000);
+        cout << "Swoj¹ barkê pozostawiam na brzegu, \n";
+        Sleep(2000);
+        cout << "Razem z Tob¹ nowy zacznê dziœ ³ów. \n";
+        Sleep(3000);
+    }
+    else
+    {
+
+     for( ; ; ) {
+      printf("DAWAJ STARA na https://github.com/owaii/Polna-Simulator-2022 !!\n");
+    }
+
+
+
+    return 0;
+}
